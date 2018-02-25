a96415372f9e2b99cf888c036b46409246221897
Wei Ren <weiren@berkeley.edu>
2018-02-24 22:03:19 -0800

    serial

---
+++ serialtest.py
@@ -0,0 +1,28 @@
+import time
+import serial
+print "Starting program"
+ser = serial.Serial('/dev/ttyS0', baudrate=9600,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS
+                    )
+time.sleep(1)
+try:
+    ser.write('Hello World\r\n')
+    ser.write('Serial Communication Using Raspberry Pi\r\n')
+    ser.write('By: Embedded Laboratory\r\n')
+    print 'Data Echo Mode Enabled'
+    while True:
+        if ser.inWaiting() > 0:
+            data = ser.read()
+            print data
+        
+except KeyboardInterrupt:
+    print "Exiting Program"
+
+except:
+    print "Error Occurs, Exiting Program"
+
+finally:
+    ser.close()
+    pass
